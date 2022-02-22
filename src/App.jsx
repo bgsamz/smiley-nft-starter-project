22e1ec9101b61872ed59de5a690822d46a7ff912
bgsamz <bgsamz@gmail.com>
2022-02-22 20:44:33 +0000

    Update title, add images, update modal.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import { ethers } from 'ethers';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import smileyNFT from './utils/SmileyNFT.json';
+import smiley1 from './assets/smiley1.svg';
+import smiley2 from './assets/smiley2.svg';
+import smiley3 from './assets/smiley3.svg';
 
 // Constants
 const TWITTER_HANDLE = 'bgsamz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/smileynft-v4';
 const TOTAL_MINT_COUNT = 50;
-const MINING_GIF_ADDRESS = "https://walfiegif.files.wordpress.com/2020/11/out-transparent-12.gif";
-const CONTRACT_ADDRESS = "0xFAAa4aC58dA87b89d0D97698140feAC5d86e1273";
+const MINING_GIF_ADDRESS = "https://thumbs.gfycat.com/ExaltedWeirdAntarcticgiantpetrel-max-1mb.gif";
+const CONTRACT_ADDRESS = "0x2cc2628bE54d84AB1B5550Ce59eD83b33F5358EC";
+// String, hex code of the chainId of the Rinkebey test network
+const CHAIN_ID = "0x4"; 
 
 const App = () => {
 
@@ -19,6 +24,8 @@ const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   // We'll open up a modal with a loading animation later
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [currentSmileyCount, setCurrentSmileyCount] = React.useState(0);
+  const [onRightChain, setIsOnRightChain] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
@@ -26,6 +33,26 @@ const App = () => {
 
   function closeModal() {
     setIsOpen(false);
+  }
+
+  const checkOnRightChain = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Please install MetaMask!");
+      setIsOnRightChain(false);
+      return;
+    }
+
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+    if (chainId !== CHAIN_ID) {
+      alert("You are not connected to the Rinkeby Test Network!");
+      setIsOnRightChain(false);
+    } else {
+      setIsOnRightChain(true);
+    }
   }
 
   const checkIfWalletIsConnected = async () => {
@@ -75,6 +102,27 @@ const App = () => {
     }
   }
 
+  const getNumSmileysMinted = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, smileyNFT.abi, signer);
+
+        let getNumTxn = await connectedContract.getNumberMinted();
+
+        console.log(`Got number minted as ${getNumTxn}`);
+        setCurrentSmileyCount(parseInt(getNumTxn));
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const setupEventListener = async () => {
     try {
       const { ethereum } = window;
@@ -86,7 +134,8 @@ const App = () => {
 
         connectedContract.on("NewSmileyNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber());
-          alert(`NFT has been minted and sent to your wallet. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          alert(`NFT has been minted and sent to your wallet. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+          getNumSmileysMinted();
         });
 
         console.log("Setup event listener.");
@@ -101,6 +150,7 @@ const App = () => {
   const askContractToMintNft = async () => {
     try {
       const { ethereum } = window;
+
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
@@ -125,14 +175,16 @@ const App = () => {
 
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} disabled={!onRightChain} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
   // Runs when page is loaded
   useEffect(() => {
+    checkOnRightChain();
     checkIfWalletIsConnected();
+    getNumSmileysMinted();
   }, [])
 
   return (
@@ -143,33 +195,48 @@ const App = () => {
           <p className="sub-text">
             A collection of uniquely generated smiley (and frowny) faces.
           </p>
+          <div className="center-container">
+            <img className="img-smiley" src={smiley1} />
+            <img className="img-smiley" src={smiley2} />
+            <img className="img-smiley" src={smiley3} />
+          </div>
+          <p className="header mint-count center-container">{currentSmileyCount}/100 Smileys minted so far.</p>
+          <div className="center-container">
             {
-              currentAccount === "" 
+              currentAccount === "" || !onRightChain
               ? (renderNotConnectedContainer()) 
               : (
-                  <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+                  <button onClick={askContractToMintNft} disabled={!onRightChain} className="cta-button connect-wallet-button">
                     Mint NFT
                   </button>
               )
             }
+          </div>
+              <button onClick={openModal} className="cta-button connect-wallet-button">
+                    TEST MODAL
+                  </button>
+          <div className="center-container">
             {
-            // <button onClick={openModal} className="cta-button connect-wallet-button">
-            // TEST OPEN MODAL
-            // </button>
+            <a href={OPENSEA_LINK}>
+              <button className="cta-button opensea-button">
+              🌊 View Collection on OpenSea 🌊
+              </button>
+            </a>
             }
+          </div>
         </div>
         <Modal
           isOpen={modalIsOpen}
-          // onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
-          // style={customStyles}
           contentLabel="Mining..."
           className="modal"
-          // overlayClassName="modal-overlay"
         >
-            <div>
-              <img className="modal-img" src={MINING_GIF_ADDRESS}/>
-            </div>
+          <div> 
+            <p className="header mining-modal-text">Mining, this may take a while...</p>
+          </div>
+          <div>
+            <img className="modal-img" src={MINING_GIF_ADDRESS}/>
+          </div>
         </Modal>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
